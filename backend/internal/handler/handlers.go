b3f923a3759aeb2e0247caaa96e6450241ba016b
小村祐輝 <y-omura@komurayuuakiranoMacBook-Pro.local>
2023-11-25 12:29:53 +0900

    modify handlers

--- backend/internal/handler/handlers.go
+++ backend/internal/handler/handlers.go
@@ -27,36 +27,3 @@ func UpdateStudent(c *gin.Context) {
 func DeleteStudent(c *gin.Context) {
 	// 学生情報の削除
 }
-
-
-// // User user type
-// type User struct {
-// 	ID   int64    `json:"id,omitempty"`
-// 	Name string   `json:"name,omitempty"`
-// 	Addr *Address `json:"addr,omitempty"`
-// }
-
-// // Address address type
-// type Address struct {
-// 	City   string
-// 	ZIP    int
-// 	LatLng [2]float64
-// }
-
-// var alex = User{
-// 	ID:   0,
-// 	Name: "Yuki Omura",
-// 	Addr: &Address{
-// 		City: "",
-// 		ZIP:  0,
-// 		LatLng: [2]float64{
-// 			0.0,
-// 			0.0,
-// 		},
-// 	},
-// }
-
-// // Hello writes a welcome string
-// func Hello() string {
-// 	return "Hello, " + alex.Name
-// }
